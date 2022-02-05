73c55a3d5b5d175c48fd00f4ecd31edbfe09ebf2
MrMisterBean <alutter03@gmail.com>
2022-02-05 05:49:46 -0500

    centered divs

--- src/Components/DisplayImage.js
+++ src/Components/DisplayImage.js
@@ -2,8 +2,8 @@ const DisplayImage = (props) => {
 
   if (props.userInput.hasOwnProperty('image')) {
     return (
-      <div class="flex flex-col justify-content bg-green-300">
-        <img class="h-auto w-3/5 bg-green-500" src={`https://ipfs.io/ipfs/${props.userInput.image.slice(7)}`} />
+      <div class="flex justify-around flex-col bg-green-300">
+        <img class="place-self-center w-4/5 bg-green-500" src={`https://ipfs.io/ipfs/${props.userInput.image.slice(7)}`} />
         <div class="grid bg-purple-300 grid-cols-2">
           {props.userInput.attributes.map((attribute) =>
             <div> {attribute.trait_type} : {attribute.value} </div>
@@ -13,7 +13,7 @@ const DisplayImage = (props) => {
     )
   } else {
     return (
-      <div> No image. {console.log(props.userInput.image)} </div>
+      <div class="bg-green-300 text-center"> No image. </div>
     )
   }
 }
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,7 +1,7 @@
 const Input = (props) => {
   return (
-    <div class="p-12 bg-blue-400">
-      <textarea class="w-3/5" rows="20" placeholder="Input Metadata..." onInput={props.handleUserInput} />
+    <div class="p-12 w-full justify-self-center bg-blue-400">
+      <textarea class="w-5/6" rows="20" placeholder="Input Metadata..." onInput={props.handleUserInput} />
     </div>
   )
 }
