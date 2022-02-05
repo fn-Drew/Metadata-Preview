a259fb1aa8db0174192458484e796071086ee401
MrMisterBean <alutter03@gmail.com>
2022-02-05 17:28:18 -0500

    now shows NFT name above image

--- src/Components/DisplayImage.js
+++ src/Components/DisplayImage.js
@@ -1,12 +1,13 @@
-const DisplayImage = (props) => {
+const DisplayImage = ({ userInput }) => {
 
-  if (props.userInput.hasOwnProperty('image')) {
+  if (userInput.hasOwnProperty('image')) {
     return (
       <div class="flex py-8 flex-col">
-        <img class="place-self-center py-8 w-4/5" src={`https://ipfs.io/ipfs/${props.userInput.image.slice(7)}`} />
-        <div class="p-14"> {props.userInput.description} </div>
+        <div class="text-center text-5xl"> {userInput.name} </div>
+        <img class="place-self-center py-8 w-4/5" src={`https://ipfs.io/ipfs/${userInput.image.slice(7)}`} />
+        <div class="p-14"> {userInput.description} </div>
         <div class="grid gap-4 grid-cols-2 text-center">
-          {props.userInput.attributes.map((attribute) =>
+          {userInput.attributes.map((attribute) =>
             <div> {attribute.trait_type} : {attribute.value} </div>
           )}
         </div>
