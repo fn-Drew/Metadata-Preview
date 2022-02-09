fadfb58f8c0066f402d83e4902d132de8de4ae46
MrMisterBean <alutter03@gmail.com>
2022-02-08 20:01:17 -0500

    added support for animation_url

--- src/Components/render/Description.js
+++ src/Components/render/Description.js
@@ -4,7 +4,7 @@ const Description = ({ description }) => {
   return (
     <div class="my-6">
       <div class="text-2xl"> Description </div>
-      <textarea class="border-none my-2 leading-relaxed font-body text-left w-4/5 resize-none" rows="5" value={description} readOnly />
+      <textarea class="bg-inherit border-none my-2 leading-relaxed font-body text-left w-4/5 resize-none" rows="5" value={description} readOnly />
     </div>
   )
 }
--- src/Components/render/Media.js
+++ src/Components/render/Media.js
@@ -1,9 +1,10 @@
 const Media = ({ userInput }) => {
   if (userInput.hasOwnProperty('image')) {
     return (
-      <div class="flex flex-col">
+      <div class="flex gap-14 flex-col">
         <div class="text-center text-3xl"> {userInput.name} </div>
         <img alt="" class="w-1/2 place-self-center" src={`https://ipfs.io/ipfs/${userInput.image.slice(7)}`} />
+        <video class="w-1/2 place-self-center" controls> <source src={`https://ipfs.io/ipfs/${userInput.animation_url.slice(7)}`} /> </video>
       </div>
     )
   } else {
