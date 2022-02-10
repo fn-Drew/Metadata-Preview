98292ee0906fc2abe2b7da40b95afc6dcd735329
MrMisterBean <alutter03@gmail.com>
2022-02-10 18:35:29 -0500

    temporarily unresponsive navbar styling

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -29,9 +29,17 @@ const NavBar = ({ setRawJsonInput, handleRawJsonInput }) => {
 
   return (
     <div>
-      <input type="submit" value="Raw JSON" onClick={showRawJsonInput} />
-      <input type="submit" value="Ipfs" onClick={showIpfsInput} />
-      <input type="submit" value="File" onClick={showFileInput} />
+      <ul class="flex border-b">
+        <li class="-mb-px-px mr-1">
+          <input class="inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" type="submit" value="Raw JSON" onClick={showRawJsonInput} />
+        </li>
+        <li class="mr-1">
+          <input class="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" type="submit" value="Ipfs" onClick={showIpfsInput} />
+        </li>
+        <li class="mr-1">
+          <input class="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" type="submit" value="File" onClick={showFileInput} />
+        </li>
+      </ul>
       <div>
         {displayRawJsonInput && <RawJsonInput handleUserInput={handleRawJsonInput} />}
         {displayIpfsInput && <IpfsInput setRawJsonInput={setRawJsonInput} />}
