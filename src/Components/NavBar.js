355ad92f21cb0d30cbdfde55eea61d2136ec3502
MrMisterBean <alutter03@gmail.com>
2022-02-10 17:58:17 -0500

    redid navbar, now functioning properly. probably a better way to do this.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,28 +5,38 @@ import IpfsInput from "./IpfsInput"
 import RawJsonInput from "./RawJsonInput"
 
 const NavBar = ({ setRawJsonInput, handleRawJsonInput }) => {
-  const [inputOption, setInputOption] = useState('RawJsonInput')
+  const [displayRawJsonInput, setDisplayRawJsonInput] = useState(false)
+  const [displayIpfsInput, setDisplayIpfsInput] = useState(false)
+  const [displayFileInput, setDisplayFileInput] = useState(false)
 
-  const Results = () => {
-    if (inputOption === "RawJsonInput") {
-      return (
-        <RawJsonInput handleUserInput={handleRawJsonInput} />
-      )
-    } else if (inputOption === "IpfsInput") {
-      return (
-        <IpfsInput setRawJsonInput={setRawJsonInput} />
-      )
-    } else if (inputOption === "FileInput") {
-      return <div> File Input Placeholder </div>
-    } else return null
+
+  const showRawJsonInput = () => {
+    setDisplayRawJsonInput(!displayRawJsonInput);
+    setDisplayIpfsInput(false);
+    setDisplayFileInput(false);
+  }
+  const showIpfsInput = () => {
+    setDisplayRawJsonInput(false);
+    setDisplayIpfsInput(!displayIpfsInput);
+    setDisplayFileInput(false);
+  }
+
+  const showFileInput = () => {
+    setDisplayRawJsonInput(false);
+    setDisplayIpfsInput(false);
+    setDisplayFileInput(!displayFileInput);
   }
 
   return (
     <div>
-      <input type="submit" value="Raw JSON" onClick={() => setInputOption("RawJsonInput")} />
-      <input type="submit" value="Ipfs" onClick={() => setInputOption("IpfsInput")} />
-      <input type="submit" value="File" onClick={() => setInputOption("FileInput")} />
-      <Results />
+      <input type="submit" value="Raw JSON" onClick={showRawJsonInput} />
+      <input type="submit" value="Ipfs" onClick={showIpfsInput} />
+      <input type="submit" value="File" onClick={showFileInput} />
+      <div>
+        {displayRawJsonInput && <RawJsonInput handleUserInput={handleRawJsonInput} />}
+        {displayIpfsInput && <IpfsInput setRawJsonInput={setRawJsonInput} />}
+        {displayFileInput && <div>File Input Placeholder</div>}
+      </div>
     </div>
   )
 }
