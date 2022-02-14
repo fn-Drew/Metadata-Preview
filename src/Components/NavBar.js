b3b34936834d7da2dd5cc2d4caa89840d78577b7
MrMisterBean <alutter03@gmail.com>
2022-02-14 01:17:01 -0500

    implemented react router and added prettierrc for tailwind css support

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,49 +1,21 @@
-import { useState } from "react"
+import { Outlet, NavLink } from "react-router-dom"
 
-import IpfsInput from "./IpfsInput"
-import RawJsonInput from "./RawJsonInput"
-import FileInput from "./FileInput"
-
-const NavBar = ({ setRawJsonInput, handleRawJsonInput }) => {
-  const [displayRawJsonInput, setDisplayRawJsonInput] = useState(false)
-  const [displayIpfsInput, setDisplayIpfsInput] = useState(false)
-  const [displayFileInput, setDisplayFileInput] = useState(false)
-
-  const showRawJsonInput = () => {
-    setDisplayRawJsonInput(true);
-    setDisplayIpfsInput(false);
-    setDisplayFileInput(false);
-  }
-  const showIpfsInput = () => {
-    setDisplayRawJsonInput(false);
-    setDisplayIpfsInput(true);
-    setDisplayFileInput(false);
-  }
-
-  const showFileInput = () => {
-    setDisplayRawJsonInput(false);
-    setDisplayIpfsInput(false);
-    setDisplayFileInput(true);
-  }
+const NavBar = () => {
 
   return (
     <div>
       <ul class="flex border-b">
         <li class="-mb-px-px mr-1">
-          <input class="inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" type="submit" value="Raw JSON" onClick={showRawJsonInput} />
+          <NavLink activeClassName="text-red-500" className="inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" to="/RawJsonInput">Raw JSON</NavLink>
         </li>
         <li class="mr-1">
-          <input class="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" type="submit" value="Ipfs" onClick={showIpfsInput} />
+          <NavLink className="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" to="/IPFSinput">Ipfs</NavLink>
         </li>
         <li class="mr-1">
-          <input class="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" type="submit" value="File" onClick={showFileInput} />
+          <NavLink className="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" to="/FileInput">File</NavLink>
         </li>
       </ul>
-      <div>
-        {displayRawJsonInput && <RawJsonInput handleUserInput={handleRawJsonInput} />}
-        {displayIpfsInput && <IpfsInput setRawJsonInput={setRawJsonInput} />}
-        {displayFileInput && <FileInput setRawJsonInput={setRawJsonInput} />}
-      </div>
+      <Outlet />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
 import App from './App'
 
+import handleRawJsonInput from "./App"
+import setRawJsonInput from "./App"
+import RawJsonInput from './Components/RawJsonInput'
+import IpfsInput from './Components/IpfsInput'
+import FileInput from "./Components/FileInput"
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="RawJsonInput" element={<RawJsonInput handleUserInput={handleRawJsonInput} />} />
+          <Route path="IPFSinput" element={<IpfsInput setRawJsonInput={setRawJsonInput} />} />
+          <Route path="FileInput" element={<FileInput setRawJsonInput={setRawJsonInput} />} />
+          <Route
+            path="*"
+            element={
+              <div class="text-5xl text-center py-8">
+                <p> There's nothing here! </p>
+              </div>
+            }
+          />
+        </Route>
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 )
