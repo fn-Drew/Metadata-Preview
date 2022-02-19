e0a92dec9e036ffac19615aca1c7ff25e2dadc88
MrMisterBean <alutter03@gmail.com>
2022-02-19 04:01:44 -0500

    updated file upload sytling and rounded input borders

--- src/Components/RawJsonInput.js
+++ src/Components/RawJsonInput.js
@@ -2,7 +2,7 @@ const RawJsonInput = ({ handleRawJsonInput }) => {
   return (
     <div class="pt-12">
       <textarea
-        class="resize-y border-[1px] border-solid border-[#69cdee] bg-[#edf9fe] p-4"
+        class="resize-y border-[1px] border-solid rounded-b-lg border-[#69cdee] bg-[#edf9fe] p-4"
         rows="5"
         placeholder="Input Metadata..."
         onInput={handleRawJsonInput}
