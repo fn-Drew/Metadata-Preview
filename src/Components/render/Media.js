ae58c1424bd45ad3f2917c4fc22b98614ddf6d5d
MrMisterBean <alutter03@gmail.com>
2022-02-20 17:26:05 -0500

    nft images now use a fixed size instead of viewport %

--- src/Components/render/Media.js
+++ src/Components/render/Media.js
@@ -8,7 +8,7 @@ const Media = ({ userInput, loading }) => {
     return (
       <div className="flex flex-col place-items-center gap-14">
         <div className="pt-8 text-center text-3xl"> Your NFT #00 </div>
-        <img alt="" className="w-1/3 shadow-lg" src='https://ipfs.io/ipfs/QmNctds8VQKxnZ5K4onxoAnUY1adsoPnbUgsKFDAPw294k' />
+        <img alt="" className="w-64 shadow-lg" src='https://ipfs.io/ipfs/QmNctds8VQKxnZ5K4onxoAnUY1adsoPnbUgsKFDAPw294k' />
       </div>
     )
 
@@ -23,7 +23,7 @@ const Media = ({ userInput, loading }) => {
       <div className="pt-8 text-center text-3xl"> {userInput.name} </div>
       {
         (!loading) ?
-          <img alt="" className="w-1/3 shadow-lg" src={nftPicture} /> :
+          <img alt="" className="w-64 shadow-lg" src={nftPicture} /> :
           <ClipLoader loading={loading} size={50} />
       }
     </div>
