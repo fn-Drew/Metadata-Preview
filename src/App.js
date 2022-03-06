7d96c3334186e3745447ca301ff0a93760089490
MrMisterBean <alutter03@gmail.com>
2022-03-05 22:16:06 -0500

    reduced max page width globally and centered page

--- src/App.js
+++ src/App.js
@@ -54,51 +54,53 @@ const App = () => {
   }
 
   return (
-    <div className="grid gap-8 h-screen w-screen grid-cols-3 bg-[#fbfdff] font-body">
+    <div className="h-screen w-screen bg-[#fbfdff] font-body">
 
-      <div className="col-span-2">
-        <div className="grid grid-flow-row pt-8 grid-rows-10 justify-items-center ">
-          <Router>
-            <NavBar />
-            <Routes>
-              <Route
-                path="RawJsonInput"
-                element={<RawJsonInput rawJson={rawJson} setRawJson={setRawJson} handleStringToJson={handleRawJsonInput} />}
-              />
-              <Route
-                path="IPFSinput"
-                element={<IpfsInput ipfsInput={ipfsInput} setIpfsInput={setIpfsInput} setRawJson={setRawJson} handleStringToJson={handleStringToJson} setJsonMetadata={setJsonMetadata} setJsonMetadata={setJsonMetadata} setLoading={setLoading} />}
-              />
-              <Route
-                path="FileInput"
-                element={<FileInput setJsonMetadata={setJsonMetadata} loading={loading} setLoading={setLoading} setRawJson={setRawJson} />}
-              />
-              <Route
-                path="*"
-                element={
-                  <div className="flex row-span-5 w-2/3 text-[#69cdee] flex-col gap-4 place-items-center resize-y border-[1px] border-solid rounded-b-lg border-[#69cdee] bg-[#edf9fe] py-12 px-[1.58rem]">
-                    <p> Choose an input method </p>
-                  </div>
-                }
-              />
-            </Routes>
-          </Router>
+      <div className="grid m-auto max-w-7xl gap-8 grid-cols-3">
+        <div className="col-span-2">
+          <div className="grid grid-flow-row pt-8 grid-rows-10 justify-items-center ">
+            <Router>
+              <NavBar />
+              <Routes>
+                <Route
+                  path="RawJsonInput"
+                  element={<RawJsonInput rawJson={rawJson} setRawJson={setRawJson} handleStringToJson={handleRawJsonInput} />}
+                />
+                <Route
+                  path="IPFSinput"
+                  element={<IpfsInput ipfsInput={ipfsInput} setIpfsInput={setIpfsInput} setRawJson={setRawJson} handleStringToJson={handleStringToJson} setJsonMetadata={setJsonMetadata} setJsonMetadata={setJsonMetadata} setLoading={setLoading} />}
+                />
+                <Route
+                  path="FileInput"
+                  element={<FileInput setJsonMetadata={setJsonMetadata} loading={loading} setLoading={setLoading} setRawJson={setRawJson} />}
+                />
+                <Route
+                  path="*"
+                  element={
+                    <div className="flex row-span-5 w-2/3 text-[#69cdee] flex-col gap-4 place-items-center resize-y border-[1px] border-solid rounded-b-lg border-[#69cdee] bg-[#edf9fe] py-12 px-[1.58rem]">
+                      <p> Choose an input method </p>
+                    </div>
+                  }
+                />
+              </Routes>
+            </Router>
+          </div>
+
+          <div>
+            <Properties setRawJson={setRawJson} jsonMetadata={jsonMetadata} setJsonMetadata={setJsonMetadata} />
+          </div>
         </div>
 
-        <div>
-          <Properties setRawJson={setRawJson} jsonMetadata={jsonMetadata} setJsonMetadata={setJsonMetadata} />
+        <div className="col-span-1">
+          <Media jsonMetadata={jsonMetadata} loading={loading} setLoading={setLoading} />
+          <AnimationUrl animationUrl={jsonMetadata.animation_url} loading={loading} />
+          <div className="text-center">
+            {/* <Description description={jsonMetadata.description} /> */}
+            <Attributes attributes={jsonMetadata.attributes} />
+          </div>
         </div>
+
       </div>
-
-      <div className="col-span-1">
-        <Media jsonMetadata={jsonMetadata} loading={loading} setLoading={setLoading} />
-        <AnimationUrl animationUrl={jsonMetadata.animation_url} loading={loading} />
-        <div className="text-center">
-          {/* <Description description={jsonMetadata.description} /> */}
-          <Attributes attributes={jsonMetadata.attributes} />
-        </div>
-      </div>
-
     </div>
   )
 }
