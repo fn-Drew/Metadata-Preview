13a84a269c167ba9598d6cbe3287baadc0962d52
MrMisterBean <alutter03@gmail.com>
2022-03-08 01:05:19 -0500

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./index.css"
 import App from "./App"
-
-import handleStringToJson from "./App"
-import setJsonMetadata from "./App"
-import RawJsonInput from "./Components/RawJsonInput"
-import IpfsInput from "./Components/IpfsInput"
-import FileInput from "./Components/FileInput"
 
 ReactDOM.render(
   <React.StrictMode>
